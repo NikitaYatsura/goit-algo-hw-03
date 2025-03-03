241e537d281f0513ee3f4fdc409d2b5c02d0cdef
NikitaYatsura <nikitayatsura@outlook.com>
2025-03-03 19:18:07 +0200

    create-func-get-number-ticket

--- task-2-random-tickets.py
+++ task-2-random-tickets.py
@@ -1,4 +1,12 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    pass
+    win_sequences = list()
+    if min < 0 or max > 1000:
+        return win_sequences
+    else:
+        while len(win_sequences) < quantity:
+            win_sequences.append(random.randrange(min, max))
+            win_sequences = set(win_sequences)
+            win_sequences = list(win_sequences)
+    return win_sequences 
