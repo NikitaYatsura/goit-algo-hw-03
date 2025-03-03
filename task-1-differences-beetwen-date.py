c0cca7b7763e6176d6a0b09bf2f6b7b7d6add619
NikitaYatsura <nikitayatsura@outlook.com>
2025-03-03 21:01:48 +0200

    add-block-try-except

--- task-1-differences-beetwen-date.py
+++ task-1-differences-beetwen-date.py
@@ -3,4 +3,12 @@ from datetime import datetime, timedelta
 # this function is calculate differences
 # between two date 
 def  get_days_from_today(date):
-    return datetime.today().date() - date.date()
+    
+    try:
+        datetime_date = datetime.strptime(date, "%Y-%m-%d")
+        return datetime.today().date() - datetime_date.date()
+    
+    except ValueError:
+        print("Incorrect date")
+        
+print(get_days_from_today("2023-03-15"))
