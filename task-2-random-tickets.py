23296957cc8b60b33608e1de6f18ebab08ed260e
NikitaYatsura <nikitayatsura@outlook.com>
2025-03-04 09:21:12 +0200

    Added check for incorrect data - when min > max and substract (max, min) < quantity

--- task-2-random-tickets.py
+++ task-2-random-tickets.py
@@ -4,11 +4,16 @@ def get_numbers_ticket(min, max, quantity):
     win_sequences = list()
     if min < 0 or max > 1000:
         return win_sequences
+    
+    elif min > max:
+        return win_sequences
+    
+    elif max - min < quantity:
+        return win_sequences
+    
     else:
         while len(win_sequences) < quantity:
             win_sequences.append(random.randrange(min, max))
             win_sequences = sorted(set(win_sequences))
-            win_sequences = list(win_sequences)
+
     return win_sequences 
-
-
