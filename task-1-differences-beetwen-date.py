261945936098bde90cb0450a655d9c73b1c417e9
NikitaYatsura <nikitayatsura@outlook.com>
2025-03-04 09:11:10 +0200

    fix return of function to integer

--- task-1-differences-beetwen-date.py
+++ task-1-differences-beetwen-date.py
@@ -6,7 +6,7 @@ def  get_days_from_today(date):
     
     try:
         datetime_date = datetime.strptime(date, "%Y-%m-%d")
-        return datetime.today().date() - datetime_date.date()
+        return (datetime.today().date() - datetime_date.date()).days
     
     except ValueError:
         print("Incorrect date")
